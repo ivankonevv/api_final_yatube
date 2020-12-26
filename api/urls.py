51554a22d651e9d3979a260937129d783885372d
Иван Конев <ivankonewvv@yandex.by>
2020-12-26 18:06:54 +0300

    ver.1.0.6

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
+
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
